4398606661417693a84297d51aad958a9ea6d8f4
MoriamAkterSwarna <moriam15-12880@diu.edu.bd>
2023-12-02 21:58:58 +0600

    zod error and validation error handlers added

--- src/app/middlewares/globalErrorHandlers.ts
+++ src/app/middlewares/globalErrorHandlers.ts
@@ -1,19 +1,56 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
+import config from '../config';
+import handleValidationError from '../errors/handleValidationError';
+import handleZodError from '../errors/handleZodError';
+import { TErrorSource } from '../interface/error';
 
-const globalErrorHandler = (
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = error.statusCode || 500;
-  const message = error.message || 'something went wrong';
+const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  let statusCode = error.statusCode || 500;
+  let message = error.message || 'something went wrong';
+
+  let errorSources: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error);
+    console.log(simplifiedError);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    // message = 'This is Zod error';
+  } else if (error?.name === 'ValidationError') {
+    // console.log('Mongoose Error');
+    const simplifiedError = handleValidationError(error);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  }
+
   return res.status(statusCode).json({
     success: false,
     message,
-    error: error,
+    // error: error,
+    errorSources,
+    stack: config.node_env === 'development' ? error?.stack : null,
   });
 };
 export default globalErrorHandler;
+
+/**
+ * ===========pattern========
+ *
+ * success
+ * message
+ * errorSources:[
+ * path: ' '
+ * message: ' '
+ * ]
+ * stack
+ */
